ddb81f2ee50a43f4ceb69a813b08760e75c23654
Michael McConkie <57379146+mcc0nkie@users.noreply.github.com>
2024-05-09 16:32:10 -0500

    Update wf.sql

--- wf.sql
+++ wf.sql
@@ -1,18 +1,22 @@
 SELECT 
     wf.sortbad,
+    -- Count unique identifier1 that dropped at this sortbad level
     COUNT(DISTINCT CASE WHEN id1_min_sortbad.sortbad = wf.sortbad THEN id1_min_sortbad.identifier1 END) AS identifier1_dropped,
+    -- Count unique identifier1 that have not failed by this sortbad level yet
     COUNT(DISTINCT CASE WHEN id1_min_sortbad.sortbad > wf.sortbad THEN id1_min_sortbad.identifier1 END) AS identifier1_remaining,
+    -- Count unique identifier1:identifier2 pairs that dropped at this sortbad level
     COUNT(DISTINCT CASE WHEN id1_id2_min_sortbad.sortbad = wf.sortbad THEN id1_id2_min_sortbad.identifier1 || ':' || id1_id2_min_sortbad.identifier2 END) AS identifier1_identifier2_dropped,
+    -- Count unique identifier1:identifier2 pairs that have not failed by this sortbad level yet
     COUNT(DISTINCT CASE WHEN id1_id2_min_sortbad.sortbad > wf.sortbad THEN id1_id2_min_sortbad.identifier1 || ':' || id1_id2_min_sortbad.identifier2 END) AS identifier1_identifier2_remaining
 FROM 
     user_work.waterfall wf
-    LEFT JOIN (
+    INNER JOIN (
         SELECT identifier1, MIN(sortbad) AS sortbad
         FROM user_work.waterfall
         GROUP BY identifier1
     ) id1_min_sortbad
     ON wf.sortbad >= id1_min_sortbad.sortbad
-    LEFT JOIN (
+    INNER JOIN (
         SELECT identifier1, identifier2, MIN(sortbad) AS sortbad
         FROM user_work.waterfall
         GROUP BY identifier1, identifier2
