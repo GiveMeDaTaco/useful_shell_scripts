3cd0ccd970a796180b070b59781195d8ad97de93
Michael McConkie <57379146+mcc0nkie@users.noreply.github.com>
2024-05-09 16:39:11 -0500

    Update wf.sql

--- wf.sql
+++ wf.sql
@@ -41,3 +41,88 @@ FROM RankedData
 GROUP BY sortbad
 ORDER BY sortbad;
 
+
+WITH 
+  -- Get the maximum sortbad for each identifier1
+  max_sortbad AS (
+    SELECT identifier1, MAX(sortbad) AS max_sortbad
+    FROM user_work.waterfall
+    GROUP BY identifier1
+  ),
+  
+  -- Get the count of identifier1 dropped at each sortbad level
+  dropped_identifier1 AS (
+    SELECT 
+      sb.sortbad, 
+      COUNT(DISTINCT mw.identifier1) AS identifier1_dropped
+    FROM 
+      max_sortbad mw
+    JOIN 
+      user_work.waterfall w ON mw.identifier1 = w.identifier1
+    WHERE 
+      w.sortbad = mw.max_sortbad
+    GROUP BY 
+      sb.sortbad
+  ),
+  
+  -- Get the count of unique identifier1 remaining at each sortbad level
+  remaining_identifier1 AS (
+    SELECT 
+      sb.sortbad, 
+      COUNT(DISTINCT w.identifier1) AS identifier1_remaining
+    FROM 
+      user_work.waterfall w
+    JOIN 
+      (SELECT DISTINCT identifier1 FROM user_work.waterfall WHERE sortbad < sb.sortbad) mw 
+      ON w.identifier1 = mw.identifier1
+    GROUP BY 
+      sb.sortbad
+  ),
+  
+  -- Get the count of identifier1:identifier2 dropped at each sortbad level
+  dropped_identifier1_identifier2 AS (
+    SELECT 
+      sb.sortbad, 
+      COUNT(DISTINCT w.identifier1, w.identifier2) AS identifier1_identifier2_dropped
+    FROM 
+      max_sortbad mw
+    JOIN 
+      user_work.waterfall w ON mw.identifier1 = w.identifier1
+    WHERE 
+      w.sortbad = mw.max_sortbad
+    GROUP BY 
+      sb.sortbad
+  ),
+  
+  -- Get the count of unique identifier1:identifier2 remaining at each sortbad level
+  remaining_identifier1_identifier2 AS (
+    SELECT 
+      sb.sortbad, 
+      COUNT(DISTINCT w.identifier1, w.identifier2) AS identifier1_identifier2_remaining
+    FROM 
+      user_work.waterfall w
+    JOIN 
+      (SELECT DISTINCT identifier1, identifier2 FROM user_work.waterfall WHERE sortbad < sb.sortbad) mw 
+      ON w.identifier1 = mw.identifier1 AND w.identifier2 = mw.identifier2
+    GROUP BY 
+      sb.sortbad
+  )
+
+SELECT 
+  sb.sortbad,
+  di1.identifier1_dropped,
+  ri1.identifier1_remaining,
+  di2.identifier1_identifier2_dropped,
+  ri2.identifier1_identifier2_remaining
+FROM 
+  (SELECT DISTINCT sortbad FROM user_work.waterfall) sb
+LEFT JOIN 
+  dropped_identifier1 di1 ON sb.sortbad = di1.sortbad
+LEFT JOIN 
+  remaining_identifier1 ri1 ON sb.sortbad = ri1.sortbad
+LEFT JOIN 
+  dropped_identifier1_identifier2 di2 ON sb.sortbad = di2.sortbad
+LEFT JOIN 
+  remaining_identifier1_identifier2 ri2 ON sb.sortbad = ri2.sortbad
+ORDER BY 
+  sb.sortbad;
